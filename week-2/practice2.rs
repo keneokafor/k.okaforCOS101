f2db96ccacd5b1bdba189f4171658949b4e3508d
k.okaforCOS101 <pivy2006gmail.com>
2023-10-21 19:41:35 +0100

    (my second commit)

---
+++ week-2/practice2.rs
@@ -0,0 +1,9 @@
+fn  main() {
+	println!(); //prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some"); //prints format some arguments
+
+	println!("My name is {}. I am the class rep of Stream 1", "Dagogo William-Jumbo" );
+}
