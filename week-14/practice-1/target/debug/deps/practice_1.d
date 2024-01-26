0d86df8ba5ee47ce08f83151ab090ba63803e41b
k.okaforCOS101 <k.okaforCOS101>
2024-01-26 23:30:54 +0100

    (My final final commit)

---
+++ week-14/practice-1/target/debug/deps/practice_1.d
@@ -0,0 +1,5 @@
+C:\Users\hp\Documents\k.okaforCOS101\week-14\practice-1\target\debug\deps\practice_1.exe: src\main.rs
+
+C:\Users\hp\Documents\k.okaforCOS101\week-14\practice-1\target\debug\deps\practice_1.d: src\main.rs
+
+src\main.rs:
---
+++ week-14/practice-1/target/debug/practice-1.d
@@ -0,0 +1,1 @@
+C:\Users\hp\Documents\k.okaforCOS101\week-14\practice-1\target\debug\practice-1.exe: C:\Users\hp\Documents\k.okaforCOS101\week-14\practice-1\src\main.rs
